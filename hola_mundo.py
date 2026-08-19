35fcb9f0b3b05d273369a7f4c0667d5138dc5bf9
camijavi <camijavinavarro@gmail.com>
2026-08-19 11:04:15 -0600

    initial commit

---
+++ hola_mundo.py
@@ -0,0 +1,3 @@
+print("Hola mundo")
+print("Estoy aprendiendo a programar en Python :P")
+print(8+8)
---
+++ presentacion.py
@@ -0,0 +1,5 @@
+nombre = "Camila Navarro"
+carrera = "Ingeniería en Sistemas"
+print (nombre + " - " + "Carrera: " + carrera)
+print("-------------------------------")
+print ("Quiero aprender más de Python, está chevere :D")
