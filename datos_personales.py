41f61f7fbb2e7ef3ac0d7f45548c7c1300e64b15
camijavi <camijavinavarro@gmail.com>
2026-08-19 14:52:57 -0600

    datos personales

---
+++ datos_personales.py
@@ -0,0 +1,7 @@
+
+print("Bienvenido, por favor ingrese los datos solicitados a continuación: ")
+nombre = input("Nombre: ")
+edad = input("Edad: ")
+carrera = input("Carrera: ")
+
+print( nombre + " " + edad + " " + carrera + " ")
